39f8442f2080300e6c8253927a625517f52d4d8b
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-01-25 11:46:47 -0800

    Add create, update and delete functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render
 
+from django.views.generic import CreateView, UpdateView, DeleteView
+
 from .models import Dog
 
 # Create your views here.
 
 
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'energyLevel', 'description', 'age']
+
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
+
+
 def home(request):
     return render(request, 'home.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def dogs_index(request):
     dogs = Dog.objects.all()
 
-    return render(request, 'dogs/index.html', { 'dogs': dogs})
+    return render(request, 'dogs/index.html', {'dogs': dogs})
+
 
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
